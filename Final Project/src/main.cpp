1a05c89bc8ff01e9fc095cc39b0c19e7f5b3736d
tinytimmer <cvtimmer@arizona.edu>
2024-04-08 12:40:15 -0700

    Inital Update CT

---
+++ Final Project/src/main.cpp
@@ -0,0 +1,10 @@
+//When you can see this in Github and can successfully edit it, please put your name and Net ID
+// Author: Carmen Timmer, 
+// Net ID: cvtimmer, 
+// Date: 4/8/2024
+// Assignment:    Final Project
+// if there is a file missing for this lab go ahead and add it in
+//----------------------------------------------------------------------//
+
+#include <Arduino.h>
+#include <avr/io.h>
